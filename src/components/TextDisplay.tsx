c355496c5707b5045e4e5e001a7815775fa0390e
dspedroborges <xpedrostewart@gmail.com>
2025-12-18 15:45:31 -0300

    feat: real support to markdown, latex and syntax highlighter

--- src/components/TextDisplay.tsx
+++ src/components/TextDisplay.tsx
@@ -1,7 +1,28 @@
-import { parseRichText } from "../utils/functions";
+import ReactMarkdown from "react-markdown"
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import "katex/dist/katex.min.css"
 
 export default function TextDisplay({ text }: { text: string }) {
-    return (
-        <span className="block" dangerouslySetInnerHTML={{ __html: parseRichText(text) }} />
-    )
+  return (
+    <ReactMarkdown
+      remarkPlugins={[remarkMath]}
+      rehypePlugins={[rehypeKatex]}
+      components={{
+        code({ className, children }) {
+          const match = /language-(\w+)/.exec(className || "")
+          return match ? (
+            <SyntaxHighlighter language={match[1]}>
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className}>{children}</code>
+          )
+        }
+      }}
+    >
+      {text}
+    </ReactMarkdown>
+  )
 }
